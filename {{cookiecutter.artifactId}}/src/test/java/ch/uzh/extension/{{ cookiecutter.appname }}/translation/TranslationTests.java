ac723597b3de3ffd55f58e3f8fedecf1a4f6d8a6
Tom Gross <itconsense@gmail.com>
2019-03-26 15:42:27 +0100

    Started with tests and coverage reporting

---
+++ {{cookiecutter.artifactId}}/src/test/java/ch/uzh/extension/{{ cookiecutter.appname }}/translation/TranslationTests.java
@@ -0,0 +1,50 @@
+package ch.uzh.extension.{{ cookiecutter.appname }}.translation;
+
+import org.junit.Test;
+
+import static org.junit.Assert.assertArrayEquals;
+import static org.junit.Assert.assertTrue;
+import static org
+
+
+public class TranslationTests {
+
+	@Test
+	public void t() {
+		assertArrayEquals(
+			TransUtil.t("save", "Save"),
+			new String[] {"save", "Save"}
+		);
+	}
+
+	@Test
+	public void testEnglish() {
+		{{ cookiecutter.appnameUpper }}EnglishTranslation trans = new {{ cookiecutter.appnameUpper }}EnglishTranslation("junit");
+		assertTrue(trans.getKeyValuePairs().containsValue("junit"));
+	}
+
+	@Test
+	public void testFrench() {
+		{{ cookiecutter.appnameUpper }}FrenchTranslation trans = new {{ cookiecutter.appnameUpper }}FrenchTranslation("junit");
+		assertTrue(trans.getKeyValuePairs().containsValue("junit"));
+	}
+
+	@Test
+	public void testGerman() {
+		{{ cookiecutter.appnameUpper }}GermanTranslation trans = new {{ cookiecutter.appnameUpper }}GermanTranslation("junit");
+		assertTrue(trans.getKeyValuePairs().containsValue("junit"));
+	}
+
+	@Test
+	public void testItalian() {
+		{{ cookiecutter.appnameUpper }}ItalianTranslation trans = new {{ cookiecutter.appnameUpper }}ItalianTranslation("junit");
+		assertTrue(trans.getKeyValuePairs().containsValue("junit"));
+	}
+
+	@Test
+	public void testFullTranslation() {
+		H5pTranslationKeyPrefix trans = new H5pTranslationKeyPrefix();
+		Translation english = trans.getInstance(LmsuzhTranslatorInternal.Language.English);
+		assertTrue(english.getKeyValuePairs().containsValue("Save"));
+	}
+}
